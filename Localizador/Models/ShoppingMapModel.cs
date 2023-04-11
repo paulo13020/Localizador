1c4b5f2694917665619d50c72c513fe9f186788f
Paulo Henrique <paulo13020@hotmail.com>
2023-04-11 08:34:00 -0300

    Primeiro commit

---
+++ Localizador/Models/ShoppingMapModel.cs
@@ -0,0 +1,32 @@
+﻿namespace Localizador.Models
+{
+    public class ShoppingMapModel
+    {
+        public class ShoppingMap
+        {
+            public List<Route> Routes { get; set; }
+
+            public ShoppingMap()
+            {
+                Routes = new List<Route>();
+            }
+        }
+
+        public class Route
+        {
+            public string Name { get; set; }
+            public List<Point> Points { get; set; }
+
+            public Route()
+            {
+                Points = new List<Point>();
+            }
+        }
+
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+        }
+    }
+}
---
+++ Localizador/wwwroot/js/shopping-map.js
@@ -0,0 +1,54 @@
+﻿let canvas = document.getElementById("shopping-map");
+let ctx = canvas.getContext("2d");
+
+// Lógica para obter o mapa de shopping e as rotas
+let shoppingMap = @Json.Serialize(Model.ShoppingMap);
+let selectedRoute = "";
+
+// Função para desenhar o mapa
+function drawMap() {
+    // Lógica para desenhar o mapa
+    // ...
+
+    // Se houver uma rota selecionada, desenha a rota
+    if (selectedRoute != "") {
+        let route = shoppingMap.Routes.find(x => x.Name == selectedRoute);
+
+        ctx.strokeStyle = "blue";
+        ctx.lineWidth = 5;
+
+        ctx.beginPath();
+        ctx.moveTo(route.Points[0].X, route.Points[0].Y);
+        for (let i = 1; i < route.Points.length; i++) {
+            ctx.lineTo(route.Points[i].X, route.Points[i].Y);
+        }
+        ctx.stroke();
+    }
+}
+
+// Função para atualizar a rota selecionada
+function updateSelectedRoute(routeName) {
+    selectedRoute = routeName;
+    drawMap();
+}
+
+// Evento de clique para selecionar uma rota
+canvas.addEventListener("click", function (e) {
+    let rect = canvas.getBoundingClientRect();
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
+
+    // Lógica para verificar se o clique está em uma rota
+    let route = shoppingMap.Routes.find(r => {
+        for (let i = 0; i < r.Points.length; i++) {
+            if (x == r.Points[i].X && y == r.Points[i].Y) {
+                return true;
+            }
+        }
+        return false;
+    });
+
+    if (route) {
+        updateSelectedRoute(route.Name);
+    }
+});
